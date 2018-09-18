9930374ec6a5eede643d4d9cbebd288fb157cd26
Augustin Copilu <gabriel.augustin1995@yahoo.com>
2018-09-18 15:33:14 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let makeNavigationStickyOnScroll = function () {
+	let $navigationMenu = $('.navigation');
+	let headerWidthOffset = 90;
+
+	if ($(window).scrollTop() >= headerWidthOffset) {
+		$navigationMenu.addClass('js-navigation-fixed');
+	}
+	else {
+		$navigationMenu.removeClass('js-navigation-fixed');
+	}
+}
+
+let resetAccordion = function() {
+	$('.accordion__item-content').css({'display':'none'});
+	$('.accordion__item-title').removeClass('accordion__item-title--black');
+	$('.accordion__item-title').find('i').remove();
+	$('.accordion__item-title').append('<i class="fas fa-angle-down"></i>');
+}
+
+let bindEventHandlers = function () {
+	$(window).on('scroll', makeNavigationStickyOnScroll);
+
+	$('.main-carousel').flickity({
+		cellAlign: 'left',
+		contain: true
+	});
+
+	$('.js-toggle-accordion').on('click', function(){
+		let $itemContent = $(this).next();
+
+		if($itemContent.css('display') === 'flex') {
+			resetAccordion();
+		} else {
+			resetAccordion();
+			$itemContent.css({'display':'flex'});
+			$(this).addClass('accordion__item-title--black');
+			$(this).find('i').remove();
+			$(this).append('<i class="fas fa-angle-up"></i>');
+		}
+	})
+}
+
+$(document).ready(function () {
+	bindEventHandlers();
+})
